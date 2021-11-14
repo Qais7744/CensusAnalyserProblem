d2df56c9494713d8f15725bf94681149e7154018
unknown <altamashbagwan1@gmail.com>
2021-11-14 16:31:10 +0530

    Added: Welcome Indian States Census Analyser Problem

---
+++ src/main/java/com/bl/designprinciple/WelcomeMessage.java
@@ -0,0 +1,7 @@
+package com.bl.designprinciple;
+
+public class WelcomeMessage {
+    public static void main(String[] args) {
+        System.out.println("Welcome Indian States Census Analyser Problem");
+    }
+}
